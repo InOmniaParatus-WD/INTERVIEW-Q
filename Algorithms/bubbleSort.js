036f714df825307723c9a8c8e0fd3001bfc8128f
InOmniaParatus-WD <78725314+InOmniaParatus-WD@users.noreply.github.com>
2023-12-20 15:42:42 +0800

    bubbleSort alg

--- Algorithms/bubbleSort.js
+++ Algorithms/bubbleSort.js
@@ -12,6 +12,7 @@ function bubbleSort(arr) {
     }
     if (noSwaps) break;
   }
+
   return arr;
 }
 
@@ -19,4 +20,4 @@ console.log(
   bubbleSort([
     1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92,
   ])
-);
+); // should return [ 1, 1, 2, 2, 4, 8, 32, 43, 43, 55, 63, 92, 123, 123, 234, 345, 5643 ]
